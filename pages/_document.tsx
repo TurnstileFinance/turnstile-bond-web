8cc63cedf1c1d0d4c98a147d42ba3db93a73e98b
JuChanLim <39957813+JuChanLim@users.noreply.github.com>
2023-02-17 16:40:21 +0900

    타이포 설정 / 폰트 설정 / body에 기본 css 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,14 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
+        {/* <link
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
           rel="stylesheet"
           type="text/css"
+        /> */}
+        <link
+          href="https://fonts.googleapis.com/css?family=Manrope"
+          rel="stylesheet"
         />
       </Head>
       <body id="app">
