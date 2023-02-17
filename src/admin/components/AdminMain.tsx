e663670577abe4d9ee71e7427a7d855c758546e4
songhoyun <itlockit@gmail.com>
2023-02-17 15:24:03 +0000

    remove unuse file

--- src/admin/components/AdminMain.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function AdminH1({ children }: { children: ReactNode }) {
-  return (
-    <main className="relative flex flex-1 flex-col space-y-4 overflow-y-auto">
-      {children}
-    </main>
-  );
-}
