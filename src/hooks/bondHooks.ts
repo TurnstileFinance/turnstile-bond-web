1a9b7a0def0fbca09263bb132658e016082cbbf9
songhoyun <itlockit@gmail.com>
2023-02-24 16:26:44 +0000

    add useGetClaimableBond useCantoBalance hooks

--- src/hooks/bondHooks.ts
+++ src/hooks/bondHooks.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { useWeb3React } from '@web3-react/core';
-import { TURNSTILE_BOND_ABI } from 'src/abi';
+import { BigNumber } from 'ethers';
+import { TURNSTILE_BOND_ABI } from 'src/abi/turnstileBond';
 import { fetcher } from 'src/api';
 import { TURNSTILE_BOND } from 'src/constants/address';
 import { NftCard } from 'src/type';
@@ -21,6 +22,28 @@ export const useCurrentBondStatus = () => {
   return useQuery<Array<NftCard>>(
     [TURNSTILE_BOND, 'currentBondStatus'],
     fetcher(library, TURNSTILE_BOND_ABI),
+    {
+      enabled: active && !!account,
+    }
+  );
+};
+
+export const useGetClaimableBond = () => {
+  const { account, active, library } = useWeb3React();
+  return useQuery<Array<NftCard>>(
+    [TURNSTILE_BOND, 'getClaimableBond', account],
+    fetcher(library, TURNSTILE_BOND_ABI),
+    {
+      enabled: active && !!account,
+    }
+  );
+};
+
+export const useCantoBalance = () => {
+  const { account, active, library } = useWeb3React();
+  return useQuery<BigNumber>(
+    ['cantoBalance', account],
+    () => library.getBalance(account),
     {
       enabled: active && !!account,
     }
