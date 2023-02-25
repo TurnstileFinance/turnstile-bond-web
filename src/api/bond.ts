98bea4370bfceed927accb3aeb116f27c616b398
songhoyun <itlockit@gmail.com>
2023-02-25 11:40:37 +0000

    add fundBond

--- src/api/bond.ts
+++ src/api/bond.ts
@@ -2,7 +2,7 @@ import { BigNumber, Contract } from 'ethers';
 import { TURNSTILE_BOND_ABI } from 'src/abi/turnstileBond';
 import { toastError } from 'src/components/Toast';
 import { TURNSTILE_BOND } from 'src/constants/address';
-import { BondCancelDto, BondStartDto } from 'src/type';
+import { BondCancelDto, BondFundDto, BondStartDto } from 'src/type';
 
 export const startBond = async (bondStartDto: BondStartDto) => {
   const {
@@ -38,6 +38,29 @@ export const cancelBond = async (bondCancelDto: BondCancelDto) => {
     return;
   }
   return contract.cancel(nftId, {
+    gasLimit: BigNumber.from(gasUnits._hex).toNumber(),
+  });
+};
+
+export const fundBond = async (bondFundDto: BondFundDto) => {
+  const {
+    library,
+    data: { nftId, amount },
+  } = bondFundDto;
+  const signer = library.getSigner();
+  const contract = new Contract(TURNSTILE_BOND, TURNSTILE_BOND_ABI, signer);
+  const gasUnits = await contract.estimateGas
+    .fund(nftId, {
+      value: amount,
+    })
+    .catch((e) => {
+      toastError(JSON.parse(JSON.stringify(e))?.reason || 'error estimateGas');
+    });
+  if (!gasUnits) {
+    return;
+  }
+  return contract.fund(nftId, {
+    value: amount,
     gasLimit: BigNumber.from(gasUnits._hex).toNumber(),
   });
 };
